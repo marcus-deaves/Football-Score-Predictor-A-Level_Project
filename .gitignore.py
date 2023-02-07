79745a7b293ef70041ed03712925a46bfe40e0b0
Marcus <m.deaves9000@gmail.com>
2023-02-07 23:07:03 +0000

    Testing

---
+++ .gitignore.py
@@ -0,0 +1,2 @@
+salt = b"$2b$06$RxspLLYEZCZhDtaEUdKLR."
+apikey = "aa9925016amshd67f07be972ba68p14f83fjsn58f25779d4b1"
--- Private_info.py
+++
@@ -1,2 +0,0 @@
-salt = b"$2b$06$RxspLLYEZCZhDtaEUdKLR."
-apikey = "aa9925016amshd67f07be972ba68p14f83fjsn58f25779d4b1"
